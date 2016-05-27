d7cff5dcc6231ea3cade408d91b0f11d7032eb12
mwjsolar <mwjsolar@163.com>
2016-05-27 13:41:00 +0800

    add mbean test

---
+++ src/main/java/base/mbean/TestMbean.java
@@ -0,0 +1,54 @@
+package base.mbean;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+
+/**
+ * see  https://blogs.oracle.com/jmxetc/entry/javax_management_standardmbean_when_and
+ *
+ * Created by mwjsolar on 16/5/27.
+ */
+public class TestMbean implements TestMbeanMBean {
+
+    private static final String NAME = "com.tool.base.test:type=testMbean";
+
+    private static TestMbean mbean = new TestMbean();
+
+    static {
+        registerMbean();
+    }
+    public static void main(String[] args) {
+        try {
+            Thread.sleep(600000);
+            mbean.print();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private  static void registerMbean() {
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        try {
+            ObjectName objectName = new ObjectName(NAME);
+            mBeanServer.registerMBean(mbean,objectName);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int testA;
+
+    public void print() {
+        System.out.println(testA);
+    }
+
+    public int getTestA() {
+        return testA;
+    }
+
+    public void setTestA(int testA) {
+        this.testA = testA;
+    }
+}
---
+++ src/main/java/base/mbean/TestMbeanMBean.java
@@ -0,0 +1,12 @@
+package base.mbean;
+
+/**
+ * Created by mwjsolar on 16/5/27.
+ */
+public interface TestMbeanMBean {
+    void print();
+
+    int getTestA();
+
+    void setTestA(int testA);
+}
